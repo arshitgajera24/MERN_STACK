ae2dcbca71455e48e7da8df17ded294ecc04e669
Arshit Gajera <arshitgajera24@gmail.com>
2025-09-28 21:55:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import { adminRoute } from './router/admin-router.js';
 const app = express();
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://mern-stack-frontend-c8bf.onrender.com",
     method: ["GET, POST, PUT, DELETE, PUT, PATCH"],
     credentials: true,
 }
